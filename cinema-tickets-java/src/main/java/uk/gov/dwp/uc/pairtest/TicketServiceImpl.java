7185619113c77a37289b25c4e5a150efe6a7d812
SatyaEserv <tetalasatya123@gmail.com>
2023-03-22 20:39:13 +0000

    Functionality for more than 20 tickets request.

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,23 +1,30 @@
 package uk.gov.dwp.uc.pairtest;
 
+import java.util.Arrays;
+
 import thirdparty.paymentgateway.TicketPaymentService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
-    /**
-     * Should only have private methods other than the one below.
-     */
+	/**
+	 * Should only have private methods other than the one below.
+	 */
 
-	 private TicketPaymentService ticketPaymentService;
+	private TicketPaymentService ticketPaymentService;
 
-	    public TicketServiceImpl(TicketPaymentService ticketPaymentService) {
-	        this.ticketPaymentService = ticketPaymentService;
-	    }
-	    
-    @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-    	 ticketPaymentService.makePayment(accountId, 20);
-    }
+	public TicketServiceImpl(TicketPaymentService ticketPaymentService) {
+		this.ticketPaymentService = ticketPaymentService;
+	}
+
+	@Override
+	public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests)
+			throws InvalidPurchaseException {
+		int totalTickets = Arrays.stream(ticketTypeRequests).mapToInt(TicketTypeRequest::getNoOfTickets).sum();
+		if (totalTickets > 20) {
+			throw new InvalidPurchaseException("Maximum of 20 tickets can be purchased at a time.");
+		}
+		ticketPaymentService.makePayment(accountId, 20);
+	}
 
 }
--- cinema-tickets-java/src/test/java/dwp/TicketServiceTest.java
+++ cinema-tickets-java/src/test/java/dwp/TicketServiceTest.java
@@ -15,23 +15,35 @@ import thirdparty.paymentgateway.TicketPaymentService;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 @ExtendWith(MockitoExtension.class)
 class TicketServiceTest {
 
-	  @Mock
-	  private TicketPaymentService ticketPaymentService;
+	@Mock
+	private TicketPaymentService ticketPaymentService;
 
-	  @InjectMocks
-	  private TicketServiceImpl ticketService;
-	  
+	@InjectMocks
+	private TicketServiceImpl ticketService;
 
-	  @Test
-	  public void testAdultRequestPayment() {
+	@Test
+	public void testAdultRequestPayment() {
 		Long accountId = 123L;
-	    TicketTypeRequest request = new TicketTypeRequest(Type.ADULT, 1); 
-	    ticketService.purchaseTickets(accountId, request);
-	    verify(ticketPaymentService).makePayment(accountId, 20);
-	  }
+		TicketTypeRequest request = new TicketTypeRequest(Type.ADULT, 1);
+		ticketService.purchaseTickets(accountId, request);
+		verify(ticketPaymentService).makePayment(accountId, 20);
+	}
+
+	@Test
+	public void testMaxTicketLimit() {
+		Long accountId = 123L;
+		TicketTypeRequest[] requests = new TicketTypeRequest[25];
+		for (int i = 0; i < 25; i++) {
+			requests[i] = new TicketTypeRequest(Type.ADULT, 1);
+		}
+		assertThrows(InvalidPurchaseException.class, () -> {
+			ticketService.purchaseTickets(accountId, requests);
+		});
+	}
 
 }
