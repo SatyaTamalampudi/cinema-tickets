e200d3848cc5cc2935583f3b7d0eaeb582a37999
SatyaEserv <tetalasatya123@gmail.com>
2023-03-22 22:27:58 +0000

    Calculating correct number of seat allocations excluding infants and requesting seat allocation service

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -3,6 +3,7 @@ package uk.gov.dwp.uc.pairtest;
 import java.util.Arrays;
 
 import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
@@ -12,25 +13,33 @@ public class TicketServiceImpl implements TicketService {
 	 * Should only have private methods other than the one below.
 	 */
 
-	private TicketPaymentService ticketPaymentService;
+	private final TicketPaymentService ticketPaymentService;
+	private final SeatReservationService seatReservationService;
 
-	public TicketServiceImpl(TicketPaymentService ticketPaymentService) {
+	public TicketServiceImpl(TicketPaymentService ticketPaymentService, SeatReservationService seatReservationService) {
 		this.ticketPaymentService = ticketPaymentService;
+		this.seatReservationService = seatReservationService;
 	}
 
 	@Override
 	public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests)
 			throws InvalidPurchaseException {
 		boolean adultTicketFound = false;
-		int totalTickets = 0;
+		int totalTickets = Arrays.stream(ticketTypeRequests).mapToInt(TicketTypeRequest::getNoOfTickets).sum();
+		int numInfants = 0;
 		int totalAmount = 0;
+
 		for (TicketTypeRequest request : ticketTypeRequests) {
 			if (request.getTicketType() == Type.ADULT) {
 				adultTicketFound = true;
 			}
-			totalTickets += request.getNoOfTickets();
+			if (request.getTicketType() == Type.INFANT) {
+				numInfants += 1;
+			}
+
 			totalAmount += request.getNoOfTickets() * getPrice(request.getTicketType());
 		}
+		int numSeats = totalTickets - numInfants;
 		if (!adultTicketFound) {
 			throw new InvalidPurchaseException(
 					"At least one Adult ticket is required to purchase Child or Infant tickets.");
@@ -38,7 +47,11 @@ public class TicketServiceImpl implements TicketService {
 		if (totalTickets > 20) {
 			throw new InvalidPurchaseException("Maximum of 20 tickets can be purchased at a time.");
 		}
+		if (numSeats > 20) {
+			throw new InvalidPurchaseException("Maximum of 20 seats can be reserved at a time.");
+		}
 		ticketPaymentService.makePayment(accountId, totalAmount);
+		seatReservationService.reserveSeat(accountId, numSeats);
 	}
 
 	private int getPrice(Type type) {
--- cinema-tickets-java/src/test/java/dwp/TicketServiceTest.java
+++ cinema-tickets-java/src/test/java/dwp/TicketServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.InjectMocks;
 
 import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
@@ -25,6 +26,9 @@ class TicketServiceTest {
 
 	@Mock
 	private TicketPaymentService ticketPaymentService;
+
+	@Mock
+	private SeatReservationService seatReservationService;
 
 	@InjectMocks
 	private TicketServiceImpl ticketService;
@@ -45,38 +49,66 @@ class TicketServiceTest {
 			ticketService.purchaseTickets(accountId, request);
 		});
 	}
-	
+
 	@Test
 	public void testMultipleRequestPayment() {
-	    Long accountId = 123L;
-	    TicketTypeRequest adultRequest = new TicketTypeRequest(Type.ADULT, 10);
-	    TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 5);
-	    TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 8);
-	    assertThrows(InvalidPurchaseException.class, () -> {
-	        ticketService.purchaseTickets(accountId, adultRequest, childRequest, infantRequest);
-	    });
-	   	}
+		Long accountId = 123L;
+		TicketTypeRequest adultRequest = new TicketTypeRequest(Type.ADULT, 10);
+		TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 5);
+		TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 8);
+		assertThrows(InvalidPurchaseException.class, () -> {
+			ticketService.purchaseTickets(accountId, adultRequest, childRequest, infantRequest);
+		});
+	}
 
 	@Test
 	public void testChildAndInfantWithoutAdult() {
-	    Long accountId = 123L;
-	    TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 5);
-	    TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 8);
-	    assertThrows(InvalidPurchaseException.class, () -> {
-	        ticketService.purchaseTickets(accountId, childRequest, infantRequest);
-	    });
-	}
-	
-	@Test
-	public void testCalculateAmountAndMakePayment() {
-	    Long accountId = 123L;
-	    TicketTypeRequest adultRequest = new TicketTypeRequest(Type.ADULT, 2);
-	    TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 1);
-	    TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 1);
-	    int totalAmount = 2 * 20 + 1 * 10 + 1 * 0;
-	    ticketService.purchaseTickets(accountId, adultRequest, childRequest, infantRequest);
-	    verify(ticketPaymentService).makePayment(accountId, totalAmount);
+		Long accountId = 123L;
+		TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 5);
+		TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 8);
+		assertThrows(InvalidPurchaseException.class, () -> {
+			ticketService.purchaseTickets(accountId, childRequest, infantRequest);
+		});
 	}
 
+	@Test
+	public void testCalculateAmountAndMakePayment() {
+		Long accountId = 123L;
+		TicketTypeRequest adultRequest = new TicketTypeRequest(Type.ADULT, 2);
+		TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 1);
+		TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 1);
+		int totalAmount = 2 * 20 + 1 * 10 + 1 * 0;
+		ticketService.purchaseTickets(accountId, adultRequest, childRequest, infantRequest);
+		verify(ticketPaymentService).makePayment(accountId, totalAmount);
+	}
+
+	@Test
+	public void testAdultRequestPaymentAndReservation() {
+		Long accountId = 123L;
+		TicketTypeRequest request = new TicketTypeRequest(Type.ADULT, 2);
+		ticketService.purchaseTickets(accountId, request);
+		verify(ticketPaymentService).makePayment(accountId, 40);
+		verify(seatReservationService).reserveSeat(accountId, 2);
+	}
+
+	@Test
+	public void testMixedRequestPaymentAndReservation() {
+		Long accountId = 123L;
+		TicketTypeRequest adultRequest = new TicketTypeRequest(Type.ADULT, 2);
+		TicketTypeRequest childRequest = new TicketTypeRequest(Type.CHILD, 1);
+		TicketTypeRequest infantRequest = new TicketTypeRequest(Type.INFANT, 1);
+		ticketService.purchaseTickets(accountId, adultRequest, childRequest, infantRequest);
+		verify(ticketPaymentService).makePayment(accountId, 50);
+		verify(seatReservationService).reserveSeat(accountId, 3);
+	}
+
+	@Test
+	public void testMaxSeatLimit() {
+		Long accountId = 123L;
+		TicketTypeRequest request = new TicketTypeRequest(Type.ADULT, 21);
+		assertThrows(InvalidPurchaseException.class, () -> {
+			ticketService.purchaseTickets(accountId, request);
+		});
+	}
 
 }
