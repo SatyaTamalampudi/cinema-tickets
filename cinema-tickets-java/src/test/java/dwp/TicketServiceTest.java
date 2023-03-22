8a3d40481a7dcc49ac0784e5c5d588ae9ecee8f5
SatyaEserv <tetalasatya123@gmail.com>
2023-03-22 11:17:08 +0000

    updated dependencis and created test file

---
+++ cinema-tickets-java/src/test/java/dwp/TicketServiceTest.java
@@ -0,0 +1,14 @@
+package dwp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TicketServiceTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
